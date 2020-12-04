0021c0d457b32fd07253e1d1ec2a865b8cf2399e
Kyril <wbpkkkk@yandex.ru>
2020-12-04 04:20:30 +0400

    data to fill database is in test.xml file

--- src/main/java/ru/magenta/RestApplication.java
+++ src/main/java/ru/magenta/RestApplication.java
@@ -4,13 +4,10 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class RestApplication extends Application {
-    @GET
-    public Response doTest() {
-        return Response.ok()
-                .entity("Service's running'")
-                .build();
-    }
+
 }
